3fd1f09338071db61470a6d26f11c68a2e0c65a1
Tushar <tusharmakadiya2000@gmail.com>
2025-01-04 00:01:54 +0530

    Fix: site name

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:3000",
             "http://192.168.1.47:3000",
+            "https://chat-app-w4lf.onrender.com/",
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
